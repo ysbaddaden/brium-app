623b42e2318257a976dd5dafae22d4f50804fba8
Julien Portalier <julien@portalier.com>
2023-05-03 23:56:52 +0200

    Run Gtk in its own thread (no more core_ext)

--- src/main.cr
+++ src/main.cr
@@ -1,7 +1,19 @@
 require "gobject/gtk"
 require "log"
-require "./core_ext/concurrency"
 require "./*"
 
+{% if !flag?(:preview_mt) %}
+  # we need preview_mt to turn-on thread safety, we can set CRYSTAL_WORKERS=1 to
+  # start only one worker/scheduler thread
+  {% raise "ERROR: you must compile the application with -Dpreview_mt" %}
+{% end %}
+
 Log.setup_from_env(default_level: :info)
-BriumApp::Application.new.run
+done = Channel(Int32).new(0)
+
+Thread.new do
+  BriumApp::Application.new.run
+  done.send(1)
+end
+
+done.receive
